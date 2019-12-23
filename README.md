9a1a445ed86c3313638a5fe8809b505122fc7408
Brandon T. Willard <brandonwillard@gmail.com>
2019-12-23 15:19:07 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# `etuples`
+
+[![Build Status](https://travis-ci.org/pythological/etuples.svg?branch=master)](https://travis-ci.org/pythological/etuples) [![Coverage Status](https://coveralls.io/repos/github/pythological/etuples/badge.svg?branch=master)](https://coveralls.io/github/pythological/etuples?branch=master) [![PyPI](https://img.shields.io/pypi/v/etuples)](https://pypi.org/project/etuples/)
+
+Python [S-expression](https://en.wikipedia.org/wiki/S-expression) emulation using tuple-like objects.
+
+## Examples
+
+
+## Installation
+
+Using `pip`:
+```bash
+pip install etuples
+```
+
+To install from source:
+```bash
+git clone git@github.com:pythological/etuples.git
+cd etuples
+pip install -r requirements.txt
+```
+
+Tests can be run with the provided `Makefile`:
+```bash
+make check
+```
---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import versioneer
+
+from os.path import exists
+from setuptools import setup
+
+setup(
+    name="etuples",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description="Python S-expression emulation using tuple-like objects.",
+    url="http://github.com/pythological/etuples",
+    maintainer="Brandon T. Willard",
+    maintainer_email="brandonwillard+etuples@gmail.com",
+    packages=["etuples"],
+    install_requires=["toolz", "multipledispatch",],
+    long_description=open("README.md").read() if exists("README.md") else "",
+    long_description_content_type="text/markdown",
+    python_requires=">=3.6",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+)
