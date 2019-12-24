82c5951c08e3e0dd083c2101bea98572d1e32850
Brandon T. Willard <brandonwillard@gmail.com>
2019-12-24 00:24:58 -0600

    Add missing versioneer files

--- etuples/__init__.py
+++ etuples/__init__.py
@@ -1,2 +1,7 @@
 from .core import etuple
 from .dispatch import etuplize, apply, rator, rands, term, operator, arguments
+
+from ._version import get_versions
+
+__version__ = get_versions()["version"]
+del get_versions
