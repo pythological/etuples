0c2fc319ced5d4c38a882c21b224f4d9a262ae00
Ryan Whitmore <whitmore5@llnl.gov>
2025-07-13 20:00:10 -0700

    Replace versioneering dynamic versioning with setuptools-scm dynamic versioning.

--- etuples/__init__.py
+++ etuples/__init__.py
@@ -1,6 +1,6 @@
-from ._version import get_versions
+import importlib.metadata
+
 from .core import etuple
 from .dispatch import apply, arguments, etuplize, operator, rands, rator, term
 
-__version__ = get_versions()["version"]
-del get_versions
+__version__ = importlib.metadata.version("etuples")
